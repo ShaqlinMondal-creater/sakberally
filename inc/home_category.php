0beca3f1eaaada31360ea27fd67236935463292c
Shaqlin <shaqlinmondal8905@gmail.com>
2025-09-06 16:13:47 +0530

    ..

--- inc/home_category.php
+++ inc/home_category.php
@@ -36,7 +36,7 @@
 
       categories.forEach(cat => {
         const name = (cat.name || '').trim();
-        const img  = cat.category_image_path
+        const img  = cat.category_image_path;
         // (cat.category_image_path && cat.category_image_path.trim())
         //               ? cat.category_image_path.trim()
         //               : 'assets/images/category_placeholder.jpg'; // add this file
