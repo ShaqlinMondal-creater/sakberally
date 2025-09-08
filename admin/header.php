b9380021b627a4002d3455cd0ced44e54960e31c
Shaqlin <shaqlinmondal8905@gmail.com>
2025-09-08 18:37:07 +0530

    ..

--- admin/header.php
+++ admin/header.php
@@ -149,6 +149,6 @@
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
             </svg>
           </button>
-          <div class="w-9 h-9 rounded-full bg-brand text-white grid place-items-center font-semibold">BK</div>
+          <div class="w-9 h-9 rounded-full bg-brand text-white grid place-items-center font-semibold">SA</div>
         </div>
       </header>
