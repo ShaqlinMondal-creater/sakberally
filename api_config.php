526212e0068344f0db1725e9742bdc1e81ae9d01
Shaqlin <shaqlinmondal8905@gmail.com>
2025-09-09 12:04:14 +0530

    ..

--- api_config.php
+++ api_config.php
@@ -1,6 +1,6 @@
 <?php
     // config.php
-    define('BASE_URL', 'https://wwww.sakberally.com/apis'); // Server
+    define('BASE_URL', 'https://www.sakberally.com/apis'); // Server
     // define('BASE_URL', 'http://localhost/sakberally/apis'); //local
     define('ADDRESS', '137, B.R.B Basu Road (Canning Street) Kolkata 7000001, West Bengal, India.');
     define('MOBILE', '9831724830');
