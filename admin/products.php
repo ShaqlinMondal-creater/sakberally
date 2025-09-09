23538c2855c58d6491c34ea950f731af7bbd8286
Shaqlin <shaqlinmondal8905@gmail.com>
2025-09-09 13:01:59 +0530

    ..

--- admin/products.php
+++ admin/products.php
@@ -131,7 +131,7 @@
 
       // const image = imgOr(p.upload_path, p.upd_link);
       const price = currency(p.price);
-      const image = p.upload_path ? p.upload_path.replace('../', '../apis') : '';
+      const image = p.upload_path ? p.upload_path.replace('../', '../apis/') : '';
 // <img src="${image}" onerror="this.src='https://via.placeholder.com/56x56?text=%20';" alt="" class="w-14 h-14 object-cover rounded bg-gray-100">
 
       tr.innerHTML = `
